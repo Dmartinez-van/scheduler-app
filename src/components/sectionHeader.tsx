40bed75a4ed43217033bf22bd4b6b835c83ff75c
David Martinez <david.antonio.martinez@gmail.com>
2023-11-21 16:19:43 -0800

    add font

---
+++ src/components/sectionHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SectionHeader = ({ title }) => {
+  return <h2>{title}</h2>
+}
+
+export default SectionHeader
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
+import { AR_One_Sans } from 'next/font/google'
+import SectionHeader from '../components/sectionHeader'
+
+const arOneSans = AR_One_Sans({
+  subsets: ['latin'],
+  variable: '--font-ar-one-sans'
+})
+
 export default function Home() {
   const sections = ['Applied', 'Interviewing', 'Offers', 'Rejected']
 
   return (
     <main>
-      <h1 className="flex justify-center p-4 outline-1 outline-dashed text-3xl">
+      <h1
+        className={`${arOneSans.variable} flex justify-center p-4 outline-1 outline-dashed text-3xl`}
+      >
         Scheduler
       </h1>
       {sections.map((title) => (
-        <h2
+        <SectionHeader
           key={title}
           className="flex outline-1 outline-dashed outline-red-300"
+          title={title}
         >
           {title}
-        </h2>
+        </SectionHeader>
       ))}
     </main>
   )
